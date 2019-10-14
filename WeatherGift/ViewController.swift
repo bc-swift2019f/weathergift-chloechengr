0e4172e59789100c94e6781f44bfc0574423a92a
chloechengr <chengsn@bc.edu>
2019-10-14 13:27:06 -0400

    Initial Commit

---
+++ WeatherGift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherGift
+//
+//  Created by Chloe Cheng on 10/14/19.
+//  Copyright © 2019 Chloe Cheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
