e951a4e7308d73431ce53f76bcf6be3dfefa39c2
chloechengr <chengsn@bc.edu>
2019-10-20 23:15:39 -0400

    7.15 Completed

---
+++ WeatherGift/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  WeatherGift
+//
+//  Created by Chloe Cheng on 10/20/19.
+//  Copyright © 2019 Chloe Cheng. All rights reserved.
+//
+
+import Foundation
+
+let urlBase = "https://api.darksky.net/forecast/"
+let urlAPIKey = "00f2a4a0cfa5d3c561a0963318727658/"
